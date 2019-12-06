ada4b6e140fe7cb87171abe8d98b5e46765cb5eb
evgeni <gg1786@rambler.ru>
2019-12-06 23:19:45 +0200

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,100 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void sendMsg(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try {
+            setButtons(sendMessage);
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //метод для приема сообщений (получение обновлений long poll (длинные запросы))
+    public void onUpdateReceived(Update update) {
+        Model model = new Model();
+        Message message = update.getMessage();
+        if (message != null && message.hasText()) {
+            switch (message.getText()) {
+                case "/help":
+                    sendMsg(message, "Чем могу помочь?");
+                    break;
+                case "/settings":
+                    sendMsg(message, "Что будем настраиывть?");
+                    break;
+                case "/tanya":
+                    sendMsg(message, "Привет Таня!!");
+                    break;
+                default:
+
+                    try {
+                        sendMsg(message, Weather.getWeather(message.getText(), model));
+                    } catch (IOException e) {
+                        sendMsg(message, "Такой город не найден");
+                    }
+
+            }
+        }
+    }
+
+    public void setButtons(SendMessage sendMessage) {
+
+        //создаем клавиатуру, эта строчка инициализирует клаву
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        //разметка клавы
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRowList = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+        keyboardFirstRow.add(new KeyboardButton("/help"));
+        keyboardFirstRow.add(new KeyboardButton("/settings"));
+
+        keyboardRowList.add(keyboardFirstRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRowList);
+
+
+    }
+
+    // для того, чтобы вернуть имя Бота, указанного при регистрации
+    public String getBotUsername() {
+        return "Mr.Black";
+    }
+
+    //это полученный Токен!!!!
+    public String getBotToken() {
+        return "911658720:AAHc8XpZMI_diXVjwA7giaChPMYCS21jQjQ";
+    }
+}
---
+++ src/main/java/Weather.java
@@ -0,0 +1,43 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Weather {
+
+    public static String getWeather(String message, Model model) throws IOException {
+
+        URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + message + "&units=metric&appid=f0e99f5caef0798c0c29c028093d2624");
+
+        Scanner in = new Scanner((InputStream) url.getContent());
+        String result = "";
+
+        while (in.hasNext()) {
+            result += in.nextLine();
+        }
+
+        JSONObject object = new JSONObject(result);
+        model.setName(object.getString("name"));
+
+        JSONObject main = object.getJSONObject("main");
+        model.setTemp(main.getDouble("temp"));
+        model.setHumidity(main.getDouble("humidity"));
+
+        JSONArray getArray = object.getJSONArray("weather");
+        for (int i = 0; i < getArray.length(); i++) {
+            JSONObject obj = getArray.getJSONObject(i);
+            model.setIcon((String) obj.get("icon"));
+            model.setMain((String) obj.get("main"));
+        }
+
+
+        return "Город : " + model.getName() + "\n" +
+                "Температура : " + model.getTemp() + "\n" +
+                "Влажность : " + model.getHumidity() + "%" + "\n" +
+                "Главное : " + model.getMain() + "\n" +
+                "Иконка : " + model.getIcon() + " .png";
+    }
+}
